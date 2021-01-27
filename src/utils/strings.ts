41c088d18ba5dcaaccd38307e88a3dd38013f6e7
Alexis Rico <sferadev@gmail.com>
2021-01-27 18:48:19 +0100

    Add string interpolation utils

---
+++ src/utils/strings.ts
@@ -0,0 +1,22 @@
+import _ from "lodash";
+
+// Match group between brackets {{template}}
+const bracketRegex = /\{\{(\w+)\}\}/g;
+
+export function interpolate(
+    string: string,
+    dictionary: Record<string, unknown>,
+    regex = bracketRegex
+): string {
+    return string.replace(regex, (_, key) => toString(dictionary[key]) ?? key);
+}
+
+export function getTemplates(string: string, regex = bracketRegex): string[] {
+    const match = string.matchAll(regex);
+    return Array.from(match).map(item => item[1]);
+}
+
+function toString(value: unknown): string {
+    if (Array.isArray(value)) return value.map(toString).join(", ");
+    return String(value);
+}
