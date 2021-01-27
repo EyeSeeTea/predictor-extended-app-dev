510f1cae6e1bbd63af36fe6af267ccfbd2dcdf2e
Alexis Rico <sferadev@gmail.com>
2021-01-27 12:26:53 +0100

    Update prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,10 @@ module.exports = {
         "no-console": "off",
         "@typescript-eslint/camelcase": "off",
         "@typescript-eslint/explicit-function-return-type": ["off"],
-        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+        ],
         "react/prop-types": "off",
         "react/display-name": "off",
         "no-unused-expressions": "off",
