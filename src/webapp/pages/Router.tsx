6a1ad12ecd49039f6d866ce6817e41e68cbac831
Alexis Rico <sferadev@gmail.com>
2021-07-18 11:56:52 +0200

    Update default router route

--- src/webapp/pages/Router.tsx
+++ src/webapp/pages/Router.tsx
@@ -1,7 +1,8 @@
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { PredictorEditPage } from "./predictor-edit/PredictorEditPage";
 import { PredictorImportPage } from "./predictor-import/PredictorImportPage";
 import { PredictorListPage } from "./predictor-list/PredictorListPage";
+import { SettingsPage } from "./settings/Settings";
 
 export const Router = () => {
     return (
@@ -12,9 +13,11 @@ export const Router = () => {
                     render={({ match }) => <PredictorEditPage type={match.params.type} id={match.params.id} />}
                 />
 
+                <Route path="/list" render={() => <PredictorListPage />} />
                 <Route path="/import" render={() => <PredictorImportPage />} />
+                <Route path="/settings" render={() => <SettingsPage />} />
 
-                <Route render={() => <PredictorListPage />} />
+                <Redirect from="*" to="/list" />
             </Switch>
         </HashRouter>
     );
